7e834c13d6ac8fb488b840fd8bad41ab627d370a
Yassin <yassin@atelier-zuppinger.ch>
2013-05-13 16:41:12 +0200

    Added caps files

---
+++ Sources/Anchor-after.js
@@ -0,0 +1,15 @@
+/*
+---
+
+name: Anchor-After
+description: This file is used as an anchor to attach script before Input/Input
+authors: AtelierZuppinger:@fingerflow
+provides: Anchor-After
+requires:
+  - Input/Anchor
+
+...
+*/
+
+
+;
---
+++ Sources/Anchor.js
@@ -0,0 +1,15 @@
+/*
+---
+
+name: Anchor
+description: This file is used as an anchor to attach script before Input/Input
+authors: AtelierZuppinger:@fingerflow
+provides: Anchor
+requires:
+  - More/Form.Validator
+
+...
+*/
+
+
+;
