9e9a072825f2f6396fea13ab6beb9ed4f8b3254f
Graphisme <graphisme@AVZ-01-iMac-27-Yassin-2.local>
2013-06-05 12:25:06 +0200

    Fix multiple upload

--- Sources/Input.File.js
+++ Sources/Input.File.js
@@ -55,11 +55,11 @@ Input.File = new Class({
 	},
 	Binds: ['storeBehaviour','fileUnlinked', 'fileDeletedCompleted', 'openLibrary', 'insertFromContent', 'insertImage', 'prepare_to_add', 'change_file', 'directUpload', 'updateImageFromLibrary', 'updateImageFromContent', 'deleteImage', 'replaceFromContent', 'directUploadFromContent', 'setImage'],
 	Implements: [Events, Options],
-	
+
 	initialize: function(field, form, options){
-		
+
 		this.setOptions(options);
-		
+
 		this.field = field;
 		var add = this.addNew = field.getElement('.img');
 		this.buttons = {
@@ -67,20 +67,16 @@ Input.File = new Class({
 			finder: add.getElement('.upload-file'),
 			add_new: add
 		};
-		this.loadImage = new Request.JSON({
-			url: '/ajax/files/get_single',
-			onSuccess: this.setImage
-		});
 		this.form = form;
 		this.fieldName = field.get('data-az-field-name');
 		this.insertArea = null;
-		
+
 		this.fileList = this.field.getElement('.file-list');
 		this.base = this.fileList.getElement('.base');
-		
-		
+
+
 		var admin = AZ.instances.cms.Admin;
-	
+
 		this.cms = {
 			admin: admin,
 			library: {
@@ -96,14 +92,14 @@ Input.File = new Class({
 				)
 			}
 		};
-		
+
 		// values send to backend
 		this.attachedFiles = this.get_initial_values();
 		this.detachedFiles = [];
-		
+
 		this.buttons.library.addEvent('click', this.insertFromContent );
 		this.buttons.finder.addEvent('click', this.directUpload );
-		
+
 		this.addEvents({
 			highlight: this.prepare_to_add,
 			removeHighlight: this.removeHighlight
@@ -113,78 +109,78 @@ Input.File = new Class({
 			'click:relay(.show-file-manager)': this.replaceFromContent,
 			'click:relay(.upload-file)': this.directUploadFromContent
 		});
-		
+
 		this.storeBehaviour( this.field );
 		this.checkAttachmentLimit();
 
 	},
-	
+
 	set: function(varName, values){
-		
+
 		this.options[varName] = values;
-		
+
 	},
-	
+
 	get_initial_values: function(){
-		
+
 		var imgs = this.fileList.getElements('div[class=attached_file] img');
 		return imgs.length ? imgs.get('data-az-file-id') : [];
-		
+
 	},
-	
+
 	insertFromContent: function(event){
 		this.openLibrary();
 		this.highlight(event);
 		// notify
 		// this.cms.admin.setInsertAction
-		
+
 		this.cms.library.instance.removeEvents('insertFile');
 		this.cms.library.instance.addEvent('insertFile', this.insertImage );
 	},
-	
+
 	replaceFromContent: function(event){
 		this.openLibrary();
 		this.highlight(event);
-		
+
 		this.cms.library.instance.removeEvents('insertFile');
 		this.cms.library.instance.addEvent('insertFile', this.updateImageFromContent );
 	},
-	
+
 	openLibrary: function(){
-		
+
 		this.cms.admin.openSection(this.cms.library.trigger.pick());
 		if( !this.cms.library.instance ){
 			this.cms.library.instance = this.cms.library.panel.retrieve('instance');
 		}
-		
+
 	},
-	
+
 	directUpload: function(event){
-		
+
 		this.insertFromContent(event);
 		this.cms.library.instance.openFinder();
 		this.cms.library.instance.autoInsert();
-		
+
 	},
-	
+
 	directUploadFromContent: function(event){
-		
+
 		this.replaceFromContent(event);
 		this.cms.library.instance.openFinder();
 		this.cms.library.instance.autoInsert();
-		
+
 	},
-	
+
 	highlight: function(event){
-		
+
 		this.cms.library.instance.fireEvent('close');
 		var btn = event.target,
 				zone = btn.getParent('.img') || btn.getParent('.attached_file');
-				
+
 		zone.addClass('highlight');
-		
+
 	},
-	
+
 	highlightAll: function(){
 		this.field.addClass('highlight');
 	},
@@ -193,11 +189,10 @@ Input.File = new Class({
 		this.field.getElements('.highlight').removeClass('highlight');
 		this.removeDropMessage();
 	},
-	
+
 	setImage: function(imageData){
-		
 		var img = this.insertArea.getElement('img');
-				
+
 		img.set({
 			src: imageData.src,
 			'data-az-file-id': imageData.id
@@ -210,17 +205,19 @@ Input.File = new Class({
 		this.insertSuccess();
 
 		this.checkAttachmentLimit();
-		
+
 	},
-	
+
 	insertImage: function(imageData){
-		
 		var div = this.base.clone();
-		
 		div.inject(this.fileList);
 		this.insertArea = div;
+
+		this.loadImage = new Request.JSON({
+			url: '/ajax/files/get_single',
+			onSuccess: this.setImage
+		});
 		this.loadImage.send(JSON.encode(imageData));
-		
 
 	},
 	updateImageFromContent: function(imageDatas){
@@ -229,53 +226,49 @@ Input.File = new Class({
 		this.detachFile(id);
 		this.insertArea = highlight;
 		this.loadImage.send(JSON.encode(imageDatas));
-		
+
 	},
-	
+
 	updateImageFromLibrary: function(event){
-		if (event.target.get('tag')=='img')
-			var img = event.target;
-		else
-			var img = event.target.getElement('img');
-		var id = img.get('data-az-file-id');
+		var id = event.target.getElement('img').get('data-az-file-id');
 		this.detachFile(id);
-		this.insertArea = img.getParent('.attached_file');
+		this.insertArea = event.target;
 		this.loadImage.send(JSON.encode(this.imageDatas));
-		
+
 	},
-	
+
 	deleteImage: function(event){
 		var attachedFile = event.target.getParent('.attached_file'),
 				img = attachedFile.getElement('img'),
 				id = img.get('data-az-file-id');
-		
+
 		this.detachFile(id);
 		attachedFile.destroy();
 		this.checkAttachmentLimit();
-		
+
 	},
-	
+
 	getHighlightedContent: function(){
 		var highlight = this.fileList.getElement('.highlight');
 		return highlight;
 	},
-	
+
 	insertSuccess: function(){
 		var lib = this.cms.library.panel.retrieve('instance');
-		
+
 		this.field.fireEvent('change');
 		lib.fireEvent('close');
 		this.detach_add();
 		this.removeDropMessage();
 		this.cms.admin.closeSections();
 	},
-	
+
 	prepare_to_add: function(file){
 		this.highlightAll();
 		this.addDropMessage();
 		this.attach_add(file);
 	},
-	
+
 	addDropMessage: function(){
 		this.field.addClass('drop-message');
 	},
@@ -284,17 +277,17 @@ Input.File = new Class({
 	},
 	attach_add: function(image){
 		this.buttons.add_new.addEvent('click', this.insertImage.pass(image) );
-		
+
 		this.imageDatas = image;
 		this.fileList.getElements('.attached_file').addEvent('click', this.updateImageFromLibrary );
 	},
-	
+
 	detach_add: function(){
 		this.buttons.add_new.removeEvents('click' );
 		this.imageDatas = null;
 		this.fileList.getElements('.attached_file').removeEvents('click');
 	},
-	
+
 	detachFile: function(imageID){
 		this.attachedFiles = this.attachedFiles.filter( function(attachedID){
 			return imageID != attachedID;
@@ -302,39 +295,39 @@ Input.File = new Class({
 		this.detachedFiles.push(imageID);
 		this.field.fireEvent('change');
 	},
-	
-	
+
+
 	change_file: function(event){
 		var img = event.target.getParent('.attached_file');
-		
+
 	},
-	
+
 	checkAttachmentLimit: function(){
-		
+
 		if( this.options.attachmentLimit ){
 			if( this.options.attachmentLimit <= this.attachedFiles.length )
 				this.hideNewFile();
 			else
 				this.showNewFile();
 		}
-		
+
 	},
-	
+
 	hideNewFile: function(){
 		this.addNew.addClass('hide');
 	},
-	
+
 	showNewFile: function(){
 		this.addNew.removeClass('hide');
 	},
-	
+
 	storeBehaviour: function(field){
 		if( !field.retrieve('behavior') )
 			field.store('behavior', this);
-		
+
 	},
 	getValue: function(){
-		
+
 		if( this.attachedFiles.length || this.detachedFiles.length )
 			return {
 				linked: this.attachedFiles,
@@ -343,32 +336,32 @@ Input.File = new Class({
 		else
 			return false;
 	},
-	
+
 	getformattedValue: function(){
 		return this.getValue();
 	},
 	restoreValue: function(values, focus){
 
 		this.field.set('value', values);
-		
+
 	},
-	
+
 	fileUnlinked: function(fileId){
-		
+
 		this.fileList.getElement('.readyToRemove').destroy();
 		this.attachedFiles = this.attachedFiles.filter( function(file){
 			return file != fileId;
 		});
 		this.dettachedFiles.push(fileId);
-		
+
 		this.field.fireEvent('change');
-		
+
 	},
-	
+
 	fileDeletedCompleted: function( response ){
 		if( response.success == true ){
 			alert('Fichier supprimé');
 		}
 	}
-	
+
 });
